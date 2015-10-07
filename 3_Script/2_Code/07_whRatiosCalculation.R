67af685c6292af7b271df6463f0b4954a4ce8789
sinhn <sinhnhn@live.com>
2015-10-07 09:43:53 +0700

    Added MY WH Ratios Calculaltion - 20151007

--- 3_Script/2_Code/07_whRatiosCalculation.R
+++ 3_Script/2_Code/07_whRatiosCalculation.R
@@ -7,19 +7,34 @@ salesDestinationLevels <- read.csv(file.path(rawDataFileWeekly,"Sales_Destinatio
                                                  "Destination_level1","Destination_level2","Destination_level3",
                                                  "Destination_level4"))
 # Load Manual Input Data - WH Allocation
+
 tempwb <- loadWorkbook(filename = file.path(manualInputFile,"WH_Allocation.xlsx"))
 whAllocation <- readWorksheet(tempwb,sheet = 1)
 
-# Matching the Sales_Destination data with WH Allocation to find the Origin Warehouse of the sales
-salesDestinationLevels <- left_join(salesDestinationLevels,whAllocation,
-                                    by = c("Destination_level3"="City.Province"))
-
-# Summarized the product sales desitnation by Origin Warehouse and the ratio of each
-whRatios <- salesDestinationLevels %>% group_by(sku) %>%
+if(venture=="Vietnam"){
+    # Matching the Sales_Destination data with WH Allocation to find the Origin Warehouse of the sales
+    salesDestinationLevels <- left_join(salesDestinationLevels,whAllocation,
+                                        by = c("Destination_level3"="City.Province"))
+    
+    # Summarized the product sales desitnation by Origin Warehouse and the ratio of each
+    whRatios <- salesDestinationLevels %>% group_by(sku) %>%
         summarize(Hanoi=sum(as.numeric(ifelse(Origin=="Ha Noi",Net_Item_sold,0))),
                   HCMC=sum(as.numeric(ifelse(Origin=="Ho Chi Minh",Net_Item_sold,0))),
                   Total=Hanoi+HCMC,
                   Ratio_Hanoi=Hanoi/Total,
                   Ratio_HCMC=HCMC/Total)
+}else if(venture=="Malaysia"){
+    # Matching the Sales_Destination data with WH Allocation to find the Origin Warehouse of the sales
+    salesDestinationLevels <- left_join(salesDestinationLevels,whAllocation,
+                                        by = c("Destination_level2"="City.Province"))
+    
+    # Summarized the product sales desitnation by Origin Warehouse and the ratio of each
+    whRatios <- salesDestinationLevels %>% group_by(sku) %>%
+        summarize(East=sum(as.numeric(ifelse(Origin=="East",Net_Item_sold,0))),
+                  West=sum(as.numeric(ifelse(Origin=="West",Net_Item_sold,0))),
+                  Total=East+West,
+                  Ratio_East=East/Total,
+                  Ratio_West=West/Total)
+}
 
 save(whRatios, file = file.path(rDataFile,"whRatios.RData"))
