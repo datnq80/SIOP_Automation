262f5ec4bbb87a792d65bdcea22023e66b94cc60
sinhn <sinhnhn@live.com>
2015-10-09 09:09:58 +0700

    Added Indonesia WHRatios - 20151009

--- 3_Script/2_Code/07_whRatiosCalculation.R
+++ 3_Script/2_Code/07_whRatiosCalculation.R
@@ -35,6 +35,21 @@ if(venture=="Vietnam"){
                   Total=East+West,
                   Ratio_East=East/Total,
                   Ratio_West=West/Total)
+}else if(venture=="Indonesia"){
+    # Matching the Sales_Destination data with WH Allocation to find the Origin Warehouse of the sales
+    salesDestinationLevels <- left_join(salesDestinationLevels,whAllocation,
+                                        by = c("Destination_level3"="Reference"))
+    
+    salesDestinationLevels <- salesDestinationLevels %>%
+        mutate(Origin=ifelse(is.na(Origin),"WH1",Origin))
+    
+    # Summarized the product sales desitnation by Origin Warehouse and the ratio of each
+    whRatios <- salesDestinationLevels %>% group_by(sku) %>%
+        summarize(WH1=sum(as.numeric(ifelse(Origin=="WH1",Net_Item_sold,0))),
+                  WH2=sum(as.numeric(ifelse(Origin=="WH2",Net_Item_sold,0))),
+                  Total=WH1+WH2,
+                  Ratio_WH1=WH1/Total,
+                  Ratio_WH2=WH2/Total)
 }
 
 save(whRatios, file = file.path(rDataFile,"whRatios.RData"))
